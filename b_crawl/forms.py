73249ff59fa78a5d82f95d0ab1015101fe01fac2
Ryoehi Jin <ryoehijin@RJnoMacBook-Air.local>
2023-08-30 10:46:39 +0900

    結果画面UI調整

--- b_crawl/forms.py
+++ b_crawl/forms.py
@@ -2,4 +2,8 @@ from django import forms
 
 
 class ScrapingForm(forms.Form):
-    url = forms.URLField(label='', required=True, widget=forms.TextInput(attrs={'class': 'custom-component','placeholder': 'スレッドURL'}))
+    url = forms.URLField(
+        label='', 
+        required=True, 
+        widget=forms.TextInput(attrs={'class': 'custom-component','placeholder': 'スレッドURL'})
+    )
