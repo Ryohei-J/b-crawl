f3c5b7e126e1860f3f52cc44148ba06581f2c906
Ryohei <r-jin@ezotec.co.jp>
2023-08-31 15:24:01 +0900

    nginx, gunicorn

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+python manage.py collectstatic --noinput
+# 環境変数のDEBUGの値がTrueの時はrunserverを、Falseの時はgunicornを実行します
+if [ $DEBUG = "True" ]
+then
+    python manage.py runserver 0.0.0.0:8000
+else
+    # gunicornを起動させる時はプロジェクト名を指定します
+    # 今回はdjangopjにします
+    gunicorn project.wsgi:application --bind 0.0.0.0:8000
+fi
--- project/urls.py
+++ project/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
-from django.conf import settings
-from django.views.static import serve 
+# from django.conf import settings
+# from django.views.static import serve 
 
 urlpatterns = [
     path('',include('b_crawl.urls')),
     path('admin/', admin.site.urls),
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    # re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
