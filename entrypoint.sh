cd2bb8b4ed603496dffe2a95df2d56d350490ee4
Ryohei Jin <ad.tgib.tsr@gmail.com>
2024-09-07 11:29:23 +0900

    軽微なフォーマット修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 # 環境変数のDEBUGの値によって開発環境か本番環境かを判定
-if [ $DEBUG = "True" ]
+if [ "$DEBUG" = "True" ]
 then
     python manage.py runserver 0.0.0.0:8000
 else
