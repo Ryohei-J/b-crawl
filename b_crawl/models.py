03b984c4b772613d69a4acac0169e3e97c1baae4
Ryohei Jin <ad.tgib.tsr@gmail.com>
2024-09-25 22:28:33 +0900

    日付カラム、X追加

--- b_crawl/models.py
+++ b_crawl/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class History(models.Model):
     title = models.CharField(max_length=255)
     url = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
