bbf9232f3522bdc538df31fa049156ff36d623b3
Ryohei Jin <ad.tgib.tsr@gmail.com>
2024-09-28 13:13:12 +0900

    削除フラグ追加、履歴取得件数と並び順調整、その他デザイン修正

--- b_crawl/models.py
+++ b_crawl/models.py
@@ -4,3 +4,4 @@ class History(models.Model):
     title = models.CharField(max_length=255)
     url = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
+    is_deleted = models.BooleanField(default=False)
--- b_crawl/views.py
+++ b_crawl/views.py
@@ -119,7 +119,7 @@ def result(request):
 # 履歴
 def history(request):
     # get
-    history_data = History.objects.all()
+    history_data = History.objects.filter(is_deleted=0).order_by('-id')[:30]
     if not history_data.exists():
         history_data = ""
     
